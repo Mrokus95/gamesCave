f2de6e0cf2d321170f22f859b7ebfc740b697d42
Lukasz Mroczkowski <luk.mroczkowski@gmail.com>
2023-07-16 15:24:27 +0200

    Added news crud features

--- gamesCave/newsRoom/urls.py
+++ gamesCave/newsRoom/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.news_list, name='news'),
-    path('<str:slug>/<int:id>', views.news_detail, name='news_detail')
+    path('detail/<str:slug>/<int:id>', views.news_detail, name='news_detail'),
+    path("add/", views.NewsCreateView.as_view(template_name="add_news.html"), name="news_add"),
+    path("update/<int:pk>/", views.NewsUpdateView.as_view(template_name="update_news.html"), name="news_update"),
+    path("delete/<int:pk>", views.NewsDeleteView.as_view(template_name="delete_news_confirmation.html"), name="news_delete"),
 ]
--- gamesCave/newsRoom/views.py
+++ gamesCave/newsRoom/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger 
 from django.contrib.auth.decorators import login_required
 from .models import News
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.template.defaultfilters import slugify
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.mixins import UserPassesTestMixin
+
+
+def staff_check(user):
+    return user.is_authenticated and user.is_staff
 
 # Create your views here.
 @login_required
@@ -30,3 +40,34 @@ def news_detail(request, slug, id):
     'news' : get_object_or_404(News, slug=slug, id=id)
     }
     return render(request, 'news_detail.html', context)
+
+class NewsCreateView(UserPassesTestMixin, CreateView):
+    model = News
+    fields = ["title", "body", "image"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user.profile
+        form.instance.slug = slugify(form.instance.title)
+        return super().form_valid(form)\
+    
+    def test_func(self):
+        return staff_check(self.request.user)
+
+class NewsUpdateView(UserPassesTestMixin, UpdateView):
+    model = News
+    fields = ["title", "body", "image"]
+    template_name = 'update_news.html'
+    
+    def test_func(self):
+        return staff_check(self.request.user)
+
+class NewsDeleteView(UserPassesTestMixin, DeleteView):
+    model = News
+    template_name = 'delete_news_confirmation.html'
+    success_url = reverse_lazy('news')  
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+    
+    def test_func(self):
+        return staff_check(self.request.user)
